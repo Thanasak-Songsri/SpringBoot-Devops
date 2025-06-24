8241611db34501db262985cc2471f6a2b88ffb95
Thanasak2003 <thanasuksongsriohm@gmail.com>
2025-06-25 00:32:30 +0700

    update 17 to 11

--- src/test/java/com/example/springboot_devops/SpringbootDevopsApplicationTests.java
+++ src/test/java/com/example/springboot_devops/SpringbootDevopsApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.springboot_devops;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringbootDevopsApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void helloEndpointShouldReturnHelloWorld() {
+		// เรียก endpoint "/"
+		ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
+		assertThat(response.getStatusCodeValue()).isEqualTo(200);
+		assertThat(response.getBody()).isEqualTo("Hello, World!");
+	}
 }
